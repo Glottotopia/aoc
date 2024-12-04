408dbb92b8c0c9c0fd33af5050bff66b139a0319
glottotopia <sebastian.nordhoff@glottotopia.de>
2024-12-04 12:09:35 +0100

    solve 04b

--- 04/04.py
+++ 04/04.py
@@ -29,3 +29,64 @@ for word in ["XMAS", "SAMX"]:
         hits += line.count(word)
 
 print(hits)
+
+
+def process(x, y):
+    new_hits = 0
+    try:
+        horizontal_mas = lists[x - 1][y] == "M" and lists[x + 1][y] == "S"
+        horizontal_sam = lists[x - 1][y] == "S" and lists[x + 1][y] == "M"
+    except IndexError:
+        horizontal_sam = False
+        horizontal_mas = False
+
+    try:
+        vertical_mas = lists[x][y - 1] == "M" and lists[x][y + 1] == "S"
+        vertical_sam = lists[x][y - 1] == "S" and lists[x + 1][y] == "M"
+    except IndexError:
+        vertical_mas = False
+        vertical_sam = False
+
+    try:
+        diag1_mas = lists[x - 1][y - 1] == "M" and lists[x + 1][y + 1] == "S"
+        diag1_sam = lists[x - 1][y - 1] == "S" and lists[x + 1][y + 1] == "M"
+    except IndexError:
+        diag1_mas = False
+        diag1_sam = False
+
+    try:
+        diag2_mas = lists[x - 1][y + 1] == "M" and lists[x + 1][y - 1] == "S"
+        diag2_sam = lists[x - 1][y + 1] == "S" and lists[x + 1][y - 1] == "M"
+    except IndexError:
+        diag2_mas = False
+        diag2_sam = False
+
+    horizontal = horizontal_sam or horizontal_mas
+    vertical = vertical_sam or vertical_mas
+    diag1 = diag1_sam or diag1_mas
+    diag2 = diag2_sam or diag2_mas
+
+    straight_cross = horizontal and vertical
+
+    st_andrews_cross = diag1 and diag2
+
+    # if straight_cross:
+    #     new_hits +=1
+    if st_andrews_cross:
+        new_hits += 1
+    if new_hits > 1:
+        print(x, y, new_hits)
+        print([lists[x - 1][y - 1], lists[x][y - 1], lists[x + 1][y - 1]]),
+        print([lists[x - 1][y], lists[x][y], lists[x + 1][y]])
+        print([lists[x - 1][y + 1], lists[x][y + 1], lists[x + 1][y + 1]])
+    return new_hits
+
+
+mas_count = 0
+for i, line in enumerate(lists):
+    for j, character in enumerate(line):
+        if character == "A":
+            new_hits = process(i, j)
+            mas_count += new_hits
+
+print(mas_count)
