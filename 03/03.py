123ba2fe9611d56cc3cf64a90dd4b64e6b518b92
glottotopia <sebastian.nordhoff@glottotopia.de>
2024-12-04 11:08:01 +0100

    blacken

--- 03/03.py
+++ 03/03.py
@@ -1,21 +1,20 @@
 import re
 
-
 MUL = re.compile("mul\(([0-9]+),([0-9]+)\)")
+
+
 def get_sum(s):
     muls = MUL.findall(s)
     # print(muls)
-    results = [int(mul[0])*int(mul[1]) for mul in muls]
+    results = [int(mul[0]) * int(mul[1]) for mul in muls]
     # print(results)
     total = sum(results)
     print(f"The sum of all multiplications is {total}")
+
 
 with open("input") as in_:
     s = in_.read()
     get_sum(s)
     do_s = s.split("do()")
-    do_s_no_don_t_s = ''.join([do.split("don't()")[0] for do in do_s])
+    do_s_no_don_t_s = "".join([do.split("don't()")[0] for do in do_s])
     get_sum(do_s_no_don_t_s)
-
-
-
